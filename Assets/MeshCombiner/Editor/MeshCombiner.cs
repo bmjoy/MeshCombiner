7666e09ef8300fee232796d9530fd29848055b18
Mogoson <mogoson@qq.com>
2017-10-15 09:40:59 -0500

    Format scripts.

--- Assets/MeshCombiner/Editor/MeshCombiner.cs
+++ Assets/MeshCombiner/Editor/MeshCombiner.cs
@@ -1,20 +1,13 @@
 /*************************************************************************
  *  Copyright (C), 2017-2018, Mogoson Tech. Co., Ltd.
- *  FileName: MeshCombiner.cs
- *  Author: Mogoson   Version: 0.1.0   Date: 8/31/2017
- *  Version Description:
- *    Internal develop version,mainly to achieve its function.
- *  File Description:
- *    Ignore.
- *  Class List:
- *    <ID>           <name>             <description>
- *     1.         MeshCombiner             Ignore.
- *  Function List:
- *    <class ID>     <name>             <description>
- *     1.
- *  History:
- *    <ID>    <author>      <time>      <version>      <description>
- *     1.     Mogoson     8/31/2017       0.1.0        Create this file.
+ *------------------------------------------------------------------------
+ *  File         :  MeshCombiner.cs
+ *  Description  :  Draw the extend editor window and combine Meshes.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/31/2017
+ *  Description  :  Initial development version.
  *************************************************************************/
 
 using System.Collections.Generic;
@@ -55,6 +48,7 @@ namespace Developer.MeshCombiner
                 "NewCombineMesh",
                 "asset",
                 "Enter a file name to save the new combine mesh.");
+
             if (newMeshPath == string.Empty)
                 return;
 
--- README.md
+++ README.md
@@ -25,4 +25,4 @@
 ![CombineMesh](./Attachments/CombineMesh.png)﻿
 
 ## Contact
-- If you have anny questions, fell free to contact me at mogoson@qq.com.
+- If you have any questions, fell free to contact me at mogoson@qq.com.
